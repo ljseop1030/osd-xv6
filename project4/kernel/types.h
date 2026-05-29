777ec42cbb5fd193c9dc630c64b2ff58e45c8ae3
Jiseop Lee <ljseop1030@gmail.com>
2026-05-29 17:19:58 +0900

    [proj4] fix PROBLEM1

--- project4/kernel/types.h
+++ project4/kernel/types.h
@@ -13,13 +13,13 @@ typedef uint64 pde_t;
     According to the README, struct page should be placed in types.h.
     However, since pagetable_t is defined in riscv.h and types.h is included beforehand,
     this could lead to an unknown type name 'pagetable_t' error.
-    We proceeded as instructed by the README for now;
-    if it triggers an error, we made it to resolve it by changing the field type to
+    First, we proceeded as instructed by the README, but it eventually caused error
+    so we made it to resolve it by changing the field type to
     uint64 *pagetable; , which is identical to pagetable_t.
 */
 // proj4 README - requires 
 struct page {
     struct page *next, *prev;
-    pagetable_t  pagetable;   // if error; uint64 *pagetable;
+    uint64 *pagetable; // error caused with pagetable_t  pagetable;  
     uint64       vaddr;
 };
